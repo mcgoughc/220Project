cef50c6c8ac8742f3853ffce885090ddbc407f53
minibutmany <minibutmany@gmail.com>
2017-11-14 10:55:32 -0500

    Added bookstore.h and bookstore.cpp

---
+++ BookStore.cpp.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Joseph Cleveland on 11/14/17.
+//
+
+#include "BookStore.h"
---
+++ BookStore.h
@@ -0,0 +1,8 @@
+//
+// Created by Joseph Cleveland on 11/14/17.
+//
+
+#ifndef INC_220PROJECT_BOOKSTORE_H
+#define INC_220PROJECT_BOOKSTORE_H
+
+#endif //INC_220PROJECT_BOOKSTORE_H
