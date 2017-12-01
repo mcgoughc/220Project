6ff667df9bd110e8e139032dfbf0d9f4b2b7c563
josephcleveland <jcleveland@ithaca.edu>
2017-12-01 16:30:52 -0500

    Person.cpp

--- Person.cpp
+++ Person.cpp
@@ -3,3 +3,15 @@
 //
 
 #include "Person.h"
+
+Person::Person(std::string firstName, std::string lastName, std::string email, std::string phoneNumber, std::string contactPreference){
+    this->firstName = firstName;
+    this->lastName = lastName;
+    this->email = email;
+    this->phoneNumber = phoneNumber;
+    this->contactPreference = contactPreference;
+}
+
+std::string Person::toString(){
+    return firstName + lastName;
+}
