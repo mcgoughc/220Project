fb721bffe755676369fa909e3cd9142b8cc8bf02
Anthony <adpizzo96@gmail.com>
2017-12-02 11:17:00 -0500

    Changed CMakeLists

--- Person.cpp
+++ Person.cpp
@@ -3,3 +3,59 @@
 //
 
 #include "Person.h"
+
+
+Person::Person(std::string nameIn, std::string phoneNumberIn, std::string emailIn, std::string commMethodIN) {
+    name = nameIn;
+    phoneNumber = phoneNumberIn;
+    email = emailIn;
+    commMethod = commMethodIN;
+}
+
+void Person::setName(std::string first, std::string last) {
+    name = last + ", " + first;
+}
+
+void Person::setPhoneNumber(std::string number) {
+    phoneNumber = number;
+}
+
+void Person::setEmail(std::string email) {
+    this->email = email;
+}
+
+void Person::setCommMethod(int commMethod) {
+    if (commMethod == 1) {
+        this->commMethod = "Phone call";
+    }
+    if (commMethod == 2) {
+        this->commMethod = "Send Text";
+    }
+    if (commMethod == 3) {
+        this->commMethod = "Send Email";
+    }
+}
+
+std::string Person::getName() {
+    return this->name;
+}
+
+std::string Person::getPhoneNumber() {
+    return this->phoneNumber;
+}
+
+std::string Person::getEmail() {
+    return this->email;
+}
+
+std::string Person::getCommMethod() {
+    return this->commMethod;
+}
+
+std::string Person::toString(Person p) {
+    std::string result = "Name: " + p.getName() +
+    "Phone Number: " + p.getPhoneNumber() +
+    "Email: " + p.getEmail() +
+    "Communication Method: " + p.getCommMethod();
+    return result;
+}
--- Person.h
+++ Person.h
@@ -5,10 +5,71 @@
 #ifndef INC_220PROJECT_PERSON_H
 #define INC_220PROJECT_PERSON_H
 
+
 #include <string>
 
 class Person {
+    private:
+        std::string name;
+        std::string phoneNumber;
+        std::string email;
+        std::string commMethod;
 
+    public:
+
+    //Constructor
+    Person(std::string nameIn, std::string phoneNumberIn, std::string emailIn, std::string commMethodIN);
+
+    /** Sets the name of the person
+     * @param first
+     * @param last
+     */
+    void setName(std::string first, std::string last);
+
+    /**
+     * Sets the phone number of the person
+     * @param number
+     */
+    void setPhoneNumber(std::string number);
+
+    /**
+     * Sets the email address of the person
+     * @param email
+     */
+    void setEmail(std::string email);
+
+    /**
+     * Sets the communication method of choice for the person
+     * @param commMethod
+     */
+    void setCommMethod(int commMethod);
+
+    /**
+     * @return name of the person
+     */
+    std::string getName();
+
+    /**
+     *
+     * @return a string containing the phone number of the person
+     */
+    std::string getPhoneNumber();
+
+    /**
+     * @return email of the person
+     */
+    std::string getEmail();
+
+    /**
+     * @return the string version of the method of communication
+     */
+    std::string getCommMethod();
+
+    /**
+     * @param Person opbject
+     * @return A string representstion of the person object
+     */
+    std::string toString(Person p);
 };
 
 
