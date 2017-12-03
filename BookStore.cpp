8f4980741498d4fc4d92e9ef05a9cb20eec30b99
Chase <mcgoughchase@gmail.com>
2017-12-03 15:29:12 -0500

    Added methods to BookStore.cpp

--- BookStore.cpp
+++ BookStore.cpp
@@ -26,8 +26,109 @@ int stringAlphaValue(std::string input){
     return stringValue;
 }
 
+/**
+ *
+ * @param A
+ * @param B
+ * @return
+ */
 bool BookStore::isAlphabeticallyGreaterThan(std::string A, std::string B) {
     int A_Value = stringAlphaValue(A);
     int B_Value = stringAlphaValue(B);
     return A > B;
+}
+
+/**
+ *
+ * @return
+ */
+std::string *BookStore::list() {
+    return nullptr;
+}
+
+/**
+ *
+ * @param title
+ * @param want
+ * @param have
+ */
+void BookStore::add(std::string title, int want, int have) {
+
+}
+
+/**
+ *
+ * @param title
+ * @param newWant
+ */
+void BookStore::setWant(std::string title, int newWant) {
+
+}
+
+/**
+ *
+ * @param title
+ * @param newHave
+ */
+void BookStore::setHave(std::string title, int newHave) {
+
+}
+
+/**
+ *
+ * @param title
+ * @return
+ */
+int BookStore::getWant(std::string title) {
+    return 0;
+}
+
+/**
+ *
+ * @param title
+ * @return
+ */
+int BookStore::getHave(std::string title) {
+    return 0;
+}
+
+/**
+ *
+ * @param title
+ * @return
+ */
+std::string *BookStore::getWaitList(std::string title) {
+    return nullptr;
+}
+
+/**
+ *
+ * @param title
+ */
+void BookStore::sell(std::string title) {
+
+}
+
+/**
+ *
+ * @param outputFile
+ */
+void BookStore::order(std::string outputFile) {
+
+}
+
+/**
+ *
+ * @param inputFile
+ */
+void BookStore::deliver(std::string inputFile) {
+
+}
+
+/**
+ *
+ * @param outputFile
+ */
+void BookStore::returnBooks(std::string outputFile) {
+
 }
