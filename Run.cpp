031a578bf536bbdb8fff714d82ab69227c83872b
Chase <mcgoughchase@gmail.com>
2017-12-09 20:04:44 -0500

    Added Run files

--- Run.cpp
+++ Run.cpp
@@ -4,4 +4,42 @@
 
 #include "Run.h"
 
+void help(BookStore& bk1){
+    //TODO
+}
 
+void inquire(BookStore& bk1){
+    //TODO
+}
+
+void list(BookStore& bk1){
+    //TODO
+}
+
+void add(BookStore& bk1){
+    //TODO
+}
+
+void modify(BookStore& bk1){
+    //TODO
+}
+
+void sell(BookStore& bk1){
+    //TODO
+}
+
+void order(BookStore& bk1){
+    //TODO
+}
+
+void delivery(BookStore& bk1){
+    //TODO
+}
+
+void returnBooks(BookStore& bk1){
+    //TODO
+}
+
+void quit(BookStore& bk1){
+    //TODO
+}
