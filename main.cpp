7b733a142a2ec27e213c0062090a812314f7b9c8
josephcleveland <jcleveland@ithaca.edu>
2017-12-13 00:36:08 -0500

    Changed get line from terminal to use the std function

--- main.cpp
+++ main.cpp
@@ -39,7 +39,9 @@ void runProgram(){
     bool run = true;
     while(run){
         std::cout << "Enter function letter key or 'h' for help: ";
-        std::string input = getLineFromTerminal();
+        std::string input;
+        std::getline(std::cin, input);
+
         if(input.length() != 1){
             std::cout << "Invalid input" << std::endl;
         }else if(input == "h"){
