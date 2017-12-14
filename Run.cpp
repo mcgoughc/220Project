cd087f2a299baa8edf1b73a66c1def29719c7abc
Chase <mcgoughchase@gmail.com>
2017-12-13 20:51:03 -0500

    fixed sell

--- Run.cpp
+++ Run.cpp
@@ -187,7 +187,6 @@ void quit(BookStore& bk1){
             printToFile(output, ',', "bookstore.txt");
         }
         std::cout << "Bookstore data saved to 'bookstore.txt'. Quitting Bookstore operation..." << std::endl;
-        delete bk1;
     }
 }
 
