e95330bd8f6141532d2f1eb22dc2f9ea63c793c6
Chase <mcgoughchase@gmail.com>
2017-12-13 20:25:21 -0500

    Changed help keywords to lowercase

--- Run.cpp
+++ Run.cpp
@@ -6,15 +6,15 @@
 #include "ArrayInventory.h"
 
 void help(){
-    std::cout << "Help(H) - provides summary of all commands" << std::endl;
-    std::cout << "Inquire(I) - display information about a book" << std::endl;
-    std::cout << "List(L) - displays a list of our entire inventory of books in alphabetical order" << std::endl;
-    std::cout << "Add(A) - adds a new book title to the inventory if it does not already exist" << std::endl;
-    std::cout << "Modify(M) - modify the want value for a specific book title" << std::endl;
-    std::cout << "Sell(S) - decreases the number of books we have of that title."
+    std::cout << "Help (h) - provides summary of all commands" << std::endl;
+    std::cout << "Inquire (i) - display information about a book" << std::endl;
+    std::cout << "List (l) - displays a list of our entire inventory of books in alphabetical order" << std::endl;
+    std::cout << "Add (a) - adds a new book title to the inventory if it does not already exist" << std::endl;
+    std::cout << "Modify (m) - modify the want value for a specific book title" << std::endl;
+    std::cout << "Sell (s) - decreases the number of books we have of that title."
     "\nIf the title of the book does not exist it will be added to the inventory."
     "\nIf the title is sold out, we will ask for your buyer information and place you on that title's waitlist" << std::endl;
-    std::cout << "Order(O) - creates a bulk purchase of additional books based on the have and want values." << std::endl;
+    std::cout << "Order (o) - creates a bulk purchase of additional books based on the have and want values." << std::endl;
 }
 
 void inquire(BookStore& bk1){
